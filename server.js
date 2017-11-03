735f9c9a1c473b301543bcf53403a1f3e9839cb3
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-11-03 17:23:54 -0500

    initial set up

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var listingRoute = require('./modules/routes/listingRoute.js');
+var mongoose = require('mongoose');
