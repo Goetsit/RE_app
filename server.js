4d4a2d4aa2d47a4fdf43522c17a8a04d51eb0f37
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-11-03 18:18:00 -0500

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var listingRoute = require('./modules/routes/listingRoute.js');
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.json()); 
+app.use(express.static('public'));
+
+
+
 var mongoose = require('mongoose');
+
+var databaseUrl = 'mongodb://localhost:27017/realestate';
+
+mongoose.connection.on('connected', function () {
+    console.log('mongoose is connected!');
+});
+
+mongoose.connection.on('error', function () {
+    console.log('mongoose connection failed');
+});
+mongoose.connect(databaseUrl);
+
+app.listen(port, function () {
+    console.log('Listening on port: ', port);
+});
