9fe091df6b28d6abb4b7a4bba0d325f5ff7ae260
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-11-03 18:18:29 -0500

    client.js controller added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,14 @@
 console.log('js sourced');
+
+
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function($routeProvider){
+    $routeProvider.when('/rent', {
+        templateUrl: 'templates/rent.html',
+        controller: 'rentController as rent'
+
+    }).when('/sale', {
+        templateUrl: 'templates/sale.html', 
+        controller: 'saleController as sale'
+    })
+});
