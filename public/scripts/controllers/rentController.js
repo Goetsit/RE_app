dace509b2f82b93d804e37e80aa478d9ee9487b7
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-11-03 18:19:13 -0500

    controllers added

---
+++ public/scripts/controllers/rentController.js
@@ -0,0 +1,7 @@
+
+myApp.controller('rentController', ['$http', function($http){
+    console.log('rentController sourced');
+
+    var vm = this;
+
+}]);
---
+++ public/scripts/controllers/saleController.js
@@ -0,0 +1,7 @@
+
+myApp.controller('saleController', ['$http', function($http){
+    console.log('saleController sourced');
+
+    var vm = this;
+
+}]);
