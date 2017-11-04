6d3580a4f78437ab7ddc2dd544394140a477f599
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-11-04 11:59:20 -0500

    get route for rentals

--- public/scripts/controllers/rentController.js
+++ public/scripts/controllers/rentController.js
@@ -3,5 +3,16 @@ myApp.controller('rentController', ['$http', function($http){
     console.log('rentController sourced');
 
     var vm = this;
+    var rentals=[];
 
+    vm.getRentals = function(){
+        $http.get('/rent').then(function (response) {
+            console.log('Success!');
+            vm.rentals = response.data;
+            console.log(vm.rentals);
+        }).catch(function (error) {
+            console.log('Failure!', error);
+        });
+    }
+    vm.getRentals();
 }]);
