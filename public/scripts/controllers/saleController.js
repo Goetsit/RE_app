c06d4b67e8c7ccd56d5bcd8986d7301412b68cf0
Laura Goetz <30483069+Goetsit@users.noreply.github.com>
2017-11-05 17:31:58 -0600

    sale controller get functioning

--- public/scripts/controllers/saleController.js
+++ public/scripts/controllers/saleController.js
@@ -3,5 +3,20 @@ myApp.controller('saleController', ['$http', function($http){
     console.log('saleController sourced');
 
     var vm = this;
+    var sales=[];
+
+    vm.getSales = function(){
+        $http.get('/sale').then(function (response) {
+            console.log('Success!');
+            vm.sales = response.data;
+            console.log(vm.sales);
+        }).catch(function (error) {
+            console.log('Failure!', error);
+        });
+    }
+    vm.getSales();
 
 }]);
+
+
+
